458a023ecc4478216e16a41af0049af0e08752b4
Zaid <zaid0999@gmail.com>
2026-08-10 12:45:53 +0530

    1-Jan day-9

--- src/components/data-binding/data-binding.jsx
+++ src/components/data-binding/data-binding.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 export function DataBinding(){
-    const [price] = useState(65);
+
+
+    const [price, setPrice] = useState(65);
+    const [name, setName] = useState('please set name')
     var username = "John"
+    
+    useEffect(()=>{
+        setPrice(5655);
+        setName('Zaid');
+    },[])
+
     return(
         <div>
             <h1>Data Binding</h1>
             <p>Hello! {username}</p>
             <input type="text" value={username} />
             <p>Price = {price}</p>
+            <p>{name}</p>
         </div>
     )
 }
